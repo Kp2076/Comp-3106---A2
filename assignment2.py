bb316999784ee917dbabe41fcde773e6696044be
Kpham-Tr <163773904+Kp2076@users.noreply.github.com>
2025-10-21 12:14:12 -0400

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import numpy as np
+import math
+
+def naive_bayes_classifier(dataset_filepath, snake_measurements):
+  x = pd.read_csv(dataset_filepath, header=None)
+  x.columns = ['class', 'length', 'weight', 'speed']
+
+  #seperate data by class
+  classes = ['anaconda', 'cobra', 'python']
+  class_data = {}
+  for cls in classes:
+    class_data[cls] = x[x['class'] == cls]
+
+  #Prior probability
+  total = len(x)
+  prior_probability = {}
+  for cls in classes:
+    prior_probability[cls] = len(class_data[cls]) / total
+
+  def gaussian(feature_x, mean_y, sigma):
+    variance = sigma**2
+    exponent = np.exp(-((x - mu)**2) / (2 * variance))
+    constant = 1 / (np.sqrt(2 * np.pi * variance))
+    return constant * variance
+
+
+
+  
+  
+  #calculate mean and std
+
+  # dataset_filepath is the full file path to a CSV file containing the dataset
+  # snake_measurements is a list of [length, weight, speed] measurements for a snake
+
+  # most_likely_class is a string indicating the most likely class, either "anaconda", "cobra", or "python"
+  # class_probabilities is a three element list indicating the probability of each class in the order [anaconda probability, cobra probability, python probability]
+  return most_likely_class, class_probabilities
